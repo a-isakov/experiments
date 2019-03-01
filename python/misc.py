90e0483150bb290647fcc9b3ffa971c3abc92dcb
Isakov <aleksey.isakov@aciworldwide.com>
2019-03-01 12:21:15 +0300

    Added example of the dict walk through

--- python/misc.py
+++ python/misc.py
@@ -12,17 +12,29 @@ def inputInt(valueName):
 
 #a = inputInt("lower multiplier")
 #b = inputInt("upper multiplier")
-s = str(input("Enter some text: "))
-sRep = ""
-while True:
-	sRep = s.replace("  ", " ", -1)
-	if s == sRep:
-		break
-	else:
-		s = sRep
-#if s[-1] == " ": s = s[:len(s) - 1]
-if s[-1] == " ": s = s[:-1]
-a = s.split(" ")
-print("Words count:", len(a))
-for i in range(len(a)):
-	print(a[i])
+def countWordsCount():
+	s = str(input("Enter some text: "))
+	sRep = ""
+	while True:
+		sRep = s.replace("  ", " ", -1)
+		if s == sRep:
+			break
+		else:
+			s = sRep
+	#if s[-1] == " ": s = s[:len(s) - 1]
+	if s[-1] == " ": s = s[:-1]
+	a = s.split(" ")
+	print("Words count:", len(a))
+	for i in range(len(a)):
+		print(a[i])
+
+def printDictTree(dicts, level = 0):
+	for item in dicts.items():
+		if type(item[1]) is not dict:
+			print("  "*level, item[0], ":", item[1])
+		else:
+			print("  "*level, item[0], ":")
+			printDictTree(item[1], level + 1)
+
+h1 = {1:"one", 2:"two", 3:"three", 4:{"a":"b", 5:{"qw":"er", "as":"zx"}}}
+printDictTree(h1)
