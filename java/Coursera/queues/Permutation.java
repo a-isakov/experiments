a4d66fbe79247db675a34b1544da52d9977e06e8
isakova <alexx.id@gmail.com>
2019-04-13 08:53:01 +0300

    Mistakes

--- java/Coursera/queues/Permutation.java
+++ java/Coursera/queues/Permutation.java
@@ -1,18 +1,19 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Permutation {
     public static void main(String[] args) {
         if (args.length > 0) {
-            final int k = Integer.parseInt(args[0]);
+            int k = Integer.parseInt(args[0]);
             RandomizedQueue<String> rq = new RandomizedQueue<>();
-            // for (int i = 1; i < args.length; i++) {
-            //     rq.enqueue(args[i]);
-            // }
-            // int i = 0;
-            // for (String s : rq) {
-            //     StdOut.println(s);
-            //     i++;
-            //     if (i == k)
-            //         break;
-            // }
+            while (!StdIn.isEmpty())
+            {
+                String line = StdIn.readString();
+                rq.enqueue(line);
+            }
+            for (int i = 0; i < k; i++) {
+                StdOut.println(rq.dequeue());
+            }
         }
     }
 }
