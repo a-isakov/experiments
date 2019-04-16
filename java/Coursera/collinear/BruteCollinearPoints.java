9aef9b56faa4de894da3c474dc454538d63b649e
isakova <alexx.id@gmail.com>
2019-04-16 11:08:13 +0300

    BruteCollinearPoints not completed

---
+++ java/Coursera/collinear/BruteCollinearPoints.java
@@ -0,0 +1,29 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class BruteCollinearPoints {
+    private LineSegment[] segments;
+
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+        if (points.length != 4)
+            throw new IllegalArgumentException("Number of points should be 4");
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+
+    }
+
+    // public static void main(String[] args) {
+    //
+    // }
+}
