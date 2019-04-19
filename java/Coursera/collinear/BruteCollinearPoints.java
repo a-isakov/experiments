6d2c0685bc13c6f920823f0f258feeeb80c25a45
isakova <alexx.id@gmail.com>
2019-04-19 11:17:35 +0300

    BruteCollinearPoints done

--- java/Coursera/collinear/BruteCollinearPoints.java
+++ java/Coursera/collinear/BruteCollinearPoints.java
@@ -14,6 +14,42 @@ import java.util.Arrays;
 public class BruteCollinearPoints {
     private LineSegment[] segments;
 
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+        if (points == null)
+            throw new IllegalArgumentException("Input param required");
+        for (int i = 0; i < points.length; i++) {
+            if (points[i] == null)
+                throw new IllegalArgumentException("Point cannot be null");
+        }
+
+        Point[] pointsCopy = new Point[points.length];
+        for (int i = 0; i < points.length; i++) {
+            pointsCopy[i] = points[i];
+        }
+        Arrays.sort(pointsCopy);
+        for (int i = 0; i < pointsCopy.length - 1; i++) {
+            if (pointsCopy[i].compareTo(pointsCopy[i + 1]) == 0)
+                throw new IllegalArgumentException("Duplicate point");
+        }
+        for (int i0 = 0; i0 < pointsCopy.length - 3; i0++) {
+            for (int i1 = i0 + 1; i1 < pointsCopy.length - 2; i1++) {
+                for (int i2 = i1 + 1; i2 < pointsCopy.length - 1; i2++) {
+                    for (int i3 = i2 + 1; i3 < pointsCopy.length; i3++) {
+                        double slope01 = pointsCopy[i0].slopeTo(pointsCopy[i1]);
+                        double slope02 = pointsCopy[i0].slopeTo(pointsCopy[i2]);
+                        double slope03 = pointsCopy[i0].slopeTo(pointsCopy[i3]);
+
+                        boolean collinear = (Double.compare(slope01, slope02) == 0 && Double.compare(slope02, slope03) == 0);
+                        if (collinear) {
+                            addSegment(pointsCopy[i0], pointsCopy[i3]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     private void addSegment(final Point point1, final Point point2) {
         if (segments == null) {
             segments = new LineSegment[1];
@@ -27,33 +63,6 @@ public class BruteCollinearPoints {
         }
 
         segments[segments.length - 1] = new LineSegment(point1, point2);
-    }
-
-    // finds all line segments containing 4 points
-    public BruteCollinearPoints(Point[] points) {
-        if (points == null)
-            throw new IllegalArgumentException("Input param required");
-        for (int i = 0; i < points.length; i++) {
-            if (points[i] == null)
-                throw new IllegalArgumentException("Point cannot be null");
-        }
-
-        Arrays.sort(points);
-        for (int i0 = 0; i0 < points.length - 3; i0++) {
-            for (int i1 = i0 + 1; i1 < points.length - 2; i1++) {
-                for (int i2 = i1 + 1; i2 < points.length - 1; i2++) {
-                    for (int i3 = i2 + 1; i3 < points.length; i3++) {
-                        double slope01 = points[i0].slopeTo(points[i1]);
-                        double slope02 = points[i0].slopeTo(points[i2]);
-                        double slope03 = points[i0].slopeTo(points[i3]);
-                        boolean collinear = (slope01 == slope02 && slope02 == slope03);
-                        if (collinear) {
-                            addSegment(points[i0], points[i3]);
-                        }
-                    }
-                }
-            }
-         }
     }
 
     // the number of line segments
@@ -83,6 +92,13 @@ public class BruteCollinearPoints {
             int y = in.readInt();
             points[i] = new Point(x, y);
         }
+        // points[n] = new Point(7000, 3000);
+        // points = new Point[5];
+        // points[0] = new Point(12426, 18881);
+        // points[1] = new Point(30474, 12435);
+        // points[2] = new Point(29140, 15542);
+        // points[3] = new Point(12426, 18881);
+        // points[4] = new Point(21042, 25628);
 
         // draw the points
         StdDraw.enableDoubleBuffering();
--- java/Coursera/collinear/FastCollinearPoints.java
+++ java/Coursera/collinear/FastCollinearPoints.java
@@ -9,6 +9,42 @@ import java.util.Arrays;
 public class FastCollinearPoints {
     private LineSegment[] segments;
 
+    // finds all line segments containing 4 or more points
+    public FastCollinearPoints(Point[] points) {
+        if (points == null)
+            throw new IllegalArgumentException("Input param required");
+        for (int i = 0; i < points.length; i++) {
+            if (points[i] == null)
+                throw new IllegalArgumentException("Point cannot be null");
+        }
+
+        Point[] pointsCopy = new Point[points.length];
+        for (int i = 0; i < points.length; i++) {
+            pointsCopy[i] = points[i];
+        }
+        Arrays.sort(pointsCopy);
+        for (int i = 0; i < pointsCopy.length - 1; i++) {
+            if (pointsCopy[i].compareTo(pointsCopy[i + 1]) == 0)
+                throw new IllegalArgumentException("Duplicate point");
+        }
+        for (int i0 = 0; i0 < pointsCopy.length - 3; i0++) {
+            for (int i1 = i0 + 1; i1 < pointsCopy.length - 2; i1++) {
+                for (int i2 = i1 + 1; i2 < pointsCopy.length - 1; i2++) {
+                    for (int i3 = i2 + 1; i3 < pointsCopy.length; i3++) {
+                        double slope01 = pointsCopy[i0].slopeTo(pointsCopy[i1]);
+                        double slope02 = pointsCopy[i0].slopeTo(pointsCopy[i2]);
+                        double slope03 = pointsCopy[i0].slopeTo(pointsCopy[i3]);
+
+                        boolean collinear = (Double.compare(slope01, slope02) == 0 && Double.compare(slope02, slope03) == 0);
+                        if (collinear) {
+                            addSegment(pointsCopy[i0], pointsCopy[i3]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     private void addSegment(final Point point1, final Point point2) {
         if (segments == null) {
             segments = new LineSegment[1];
@@ -22,33 +58,6 @@ public class FastCollinearPoints {
         }
 
         segments[segments.length - 1] = new LineSegment(point1, point2);
-    }
-
-    // finds all line segments containing 4 or more points
-    public FastCollinearPoints(Point[] points) {
-        if (points == null)
-            throw new IllegalArgumentException("Input param required");
-        for (int i = 0; i < points.length; i++) {
-            if (points[i] == null)
-                throw new IllegalArgumentException("Point cannot be null");
-        }
-
-        Arrays.sort(points);
-        for (int i0 = 0; i0 < points.length - 3; i0++) {
-            for (int i1 = i0 + 1; i1 < points.length - 2; i1++) {
-                for (int i2 = i1 + 1; i2 < points.length - 1; i2++) {
-                    for (int i3 = i2 + 1; i3 < points.length; i3++) {
-                        double slope01 = points[i0].slopeTo(points[i1]);
-                        double slope02 = points[i0].slopeTo(points[i2]);
-                        double slope03 = points[i0].slopeTo(points[i3]);
-                        boolean collinear = (slope01 == slope02 && slope02 == slope03);
-                        if (collinear) {
-                            addSegment(points[i0], points[i3]);
-                        }
-                    }
-                }
-            }
-        }
     }
 
     // the number of line segments
