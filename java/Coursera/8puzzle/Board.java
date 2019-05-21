bb7eb90b3e8d165a3c3fca447a7d9a0ce7dff47c
isakova <alexx.id@gmail.com>
2019-05-21 11:46:26 +0300

    Revert "LeetCode Two Sum solution"
    This reverts commit 4e5ff38a2885449997897790a022139f5c2e1737.

--- java/Coursera/8puzzle/Board.java
+++ java/Coursera/8puzzle/Board.java
@@ -4,9 +4,6 @@ public class Board {
     // construct a board from an n-by-n array of blocks
     // (where blocks[i][j] = block in row i, column j)
     public Board(int[][] blocks) {
-        if (blocks == null)
-            throw new IllegalArgumentException("Null argument");
-
         board = new int[blocks.length][blocks[0].length];
         for (int i = 0; i < blocks.length; i++) {
             for (int j = 0; j < blocks[i].length; j++) {
@@ -25,8 +22,8 @@ public class Board {
         int h = 0;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                int index = i*board.length + j + 1;
-                if (board[i][j] != index && board[i][j] != 0)
+                int index = j*board.length + i;
+                if (board[i][j] == index)
                     h++;
             }
         }
@@ -38,12 +35,10 @@ public class Board {
         int m = 0;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == 0)
-                    continue;
-                int index = i*board.length + j + 1;
+                int index = j*board.length + i;
                 int x = index % board.length;
                 int y = index/board.length;
-                m += Math.abs(x - j) + Math.abs(y - i);
+                m += Math.abs(x - i) + Math.abs(y - j);
             }
         }
         return m;
@@ -64,37 +59,14 @@ public class Board {
 
     // a board that is obtained by exchanging any pair of blocks
     public Board twin() {
-        // Most probably it is incorrect
-        if (board[0][0] == 0) {
-            int value = board[0][1];
-            board[0][1] = board[0][2];
-            board[0][2] = value;
-        } else if (board[0][1] == 0) {
-            int value = board[0][0];
-            board[0][0] = board[0][2];
-            board[0][2] = value;
-        } else {
-            int value = board[0][0];
-            board[0][0] = board[0][1];
-            board[0][1] = value;
-        }
+        // TODO:
         return null;
     }
 
     // does this board equal y?
     public boolean equals(Object y) {
-        if (y == this) return true;
-        if (y == null) return false;
-        if (y.getClass() != this.getClass()) return false;
-
-        int[][] anotherBoard = (int[][]) y;
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] != anotherBoard[i][j])
-                    return false;
-            }
-        }
-        return true;
+        // TODO:
+        return false;
     }
 
     // all neighboring boards
@@ -105,15 +77,8 @@ public class Board {
 
     // string representation of this board (in the output format specified below)
     public String toString() {
-        StringBuilder s = new StringBuilder();
-        s.append(board.length + "\n");
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                s.append(String.format("%2d ", board[i][j]));
-            }
-            s.append("\n");
-        }
-        return s.toString();
+        // TODO:
+        return "";
     }
 
     public static void main(String[] args) {
