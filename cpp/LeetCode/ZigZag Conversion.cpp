893bcb09a45ab5f0cdbd419e8236e9186c86f6b0
isakova <alexx.id@gmail.com>
2019-05-24 17:39:11 +0300

    LeetCode: ZigZag Conversion

---
+++ cpp/LeetCode/ZigZag Conversion.cpp
@@ -0,0 +1,74 @@
+/*
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+Example 1:
+
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+Example 2:
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+*/
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 0)
+            return "";
+        if (numRows == 1)
+            return s;
+        
+        int mids = numRows > 2 ? numRows - 2 : 0;
+        int curveSize = numRows + mids;
+        
+        string result = "";
+        for (int row = 0; row < numRows; row++)
+        {
+            int col = 0;
+            int index = 0;
+            while (index < s.length())
+            {
+                if (row == 0)
+                {
+                    // First row
+                    index = col*curveSize;
+                    result += s[index];
+                }
+                else if (row == numRows - 1)
+                {
+                    // Last row
+                    index = col*curveSize + numRows - 1;
+                    result += s[index];
+                }
+                else
+                {
+                    // Mid row
+                    
+                }
+                col++;
+            }
+        }
+    }
+};
