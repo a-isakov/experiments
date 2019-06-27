dc4f617d2169ebfaf25002b7245194d5f7dae900
isakova <aleksey.isakov@aciworldwide.com>
2019-06-27 16:50:54 +0300

    KdTree just few basic changes

--- java/Coursera/kdtree/PointSET.java
+++ java/Coursera/kdtree/PointSET.java
@@ -4,7 +4,11 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.SET;
+
 public class PointSET {
+    private SET<Point2D> points;
+
     // construct an empty set of points
     public PointSET() {
 
@@ -12,36 +16,51 @@ public class PointSET {
 
     // is the set empty?
     public boolean isEmpty() {
-        return false;
+        return points.isEmpty();
     }
 
     // number of points in the set
     public int size() {
-        return 0;
+        return points.size();
     }
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
+        if (p == null)
+            throw new java.lang.IllegalArgumentException();
 
+        if (!points.contains(p))
+            points.add(p);
     }
 
     // does the set contain point p?
     public boolean contains(Point2D p) {
-        return false;
+        if (p == null)
+            throw new java.lang.IllegalArgumentException();
+
+        return points.contains(p);
     }
 
     // draw all points to standard draw
     public void draw() {
-
+        for (Point2D point: points) {
+            point.draw();
+        }
     }
 
     // all points that are inside the rectangle (or on the boundary)
     public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null)
+            throw new java.lang.IllegalArgumentException();
+
         return null;
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
+        if (p == null)
+            throw new java.lang.IllegalArgumentException();
+
         return null;
     }
 
