32f371c9a2a9fc33a36727ca12f8acd00cdaacbd
Alexey Isakov <alexx.id@gmail.com>
2019-07-14 21:51:11 +0300

    uniquePaths completed

--- cpp/LeetCode/Unique Paths.hpp
+++ cpp/LeetCode/Unique Paths.hpp
@@ -19,9 +19,41 @@ Input: m = 7, n = 3
 Output: 28
 */
 
+/*
+Runtime: 0 ms, faster than 100.00%. Next time 4ms
+Memory Usage: 8.3 MB, less than 70.14%
+*/
+
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
 class Solution {
 public:
 	int uniquePaths(int m, int n) {
-
+		if (!m || !n)
+			return 0;
+		if (n == 1 || m == 1)
+			return 1;
+		if (m == 2)
+			return n;
+		if (n == 2)
+			return m;
+		int result = n;
+		vector<int> helper2(n - 1);
+		vector<int> helper1(n - 1, 1);
+		for (int x = 2; x < m; x++)
+		{
+			helper2[0] = 1;
+			for (int y = 1; y < n - 1; y++)
+			{
+				helper2[y] = accumulate(helper1.begin(), helper1.begin() + y + 1, 0);
+			}
+			helper1 = helper2;
+			result += accumulate(helper1.begin(), helper1.end(), 0);
+		}
+		
+		return result;
 	}
 };
