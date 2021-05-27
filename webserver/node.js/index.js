a15db402671f0945cc7051734e23abab635ca23e
Aleksey Isakov <aleksey.isakov@akbarsdigital.ru>
2021-05-27 16:34:17 +0300

    fake method

--- webserver/node.js/index.js
+++ webserver/node.js/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express();
+
 // const bodyParser = require('body-parser')
 // app.use(bodyParser.json());
 
@@ -10,6 +11,18 @@ const PORT = 8080;
 app.get('/', async (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 })
+
+app.get('/api/v1/get-random-array', async (req, res) => {
+    const max_random = req.query.max || 1000;
+    const array_items_count = req.query.count || 10;
+
+    res_arr = [];
+    for(i = 0; i < array_items_count; i++) {
+        res_arr[i] = Math.round(Math.random() * max_random);
+    }
+    res.contentType('application/json');
+    res.json({array: res_arr});
+});
 
 app.listen(PORT, () => {
     console.log('Listening on port', PORT);
