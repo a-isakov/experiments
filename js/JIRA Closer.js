9baa84ef9aaad511289d11d4c70bd09b654214a0
Aleksey Isakov <aleksey.isakov@akbarsdigital.ru>
2021-07-22 10:18:13 +0300

    Updated to use fetch. Doesn't work yet

--- js/JIRA Closer.js
+++ js/JIRA Closer.js
@@ -71,7 +71,7 @@
     function customCloserListener(jiraKey, complete) {
         if (confirm('Are you sure you want to close the issue with all subtasks?')) {
             Promise.all([
-                closeSubtasks(jiraKey, complete),
+                // closeSubtasks(jiraKey, complete),
                 closeJira(jiraKey, complete)
             ]).then(() => console.log('done'))
             // window.location.reload();
@@ -92,96 +92,85 @@
         request.send(); // retrieve list of subtasks
     }
 
-    function closeJira(jiraKey, complete) {
+    async function closeJira(jiraKey, complete) {
         console.log('This will close ' + jiraKey + ' as ' + complete);
-        let request = new XMLHttpRequest();
-        request.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                const jiraIssue = JSON.parse(request.responseText);
-                const status = jiraIssue['fields']['status']['id'];
-                const type = jiraIssue['fields']['issuetype']['id'];
-                
-                // console.log(jiraIssue);
-                if (status != STATUS_CLOSED) {
-                    closeByWorkflow(jiraKey, type, status, complete);
-                }
+        const response = await fetch('/rest/api/2/issue/' + jiraKey + '?fields=issuetype,status'); // retrieve issue details to close
+        if (response.status == 200) {
+            const jiraIssue = await response.json();
+            // console.log(jiraIssue);
+            const status = jiraIssue['fields']['status']['id'];
+            const type = jiraIssue['fields']['issuetype']['id'];
+            if (status != STATUS_CLOSED) {
+                await closeByWorkflow(jiraKey, type, status, complete);
             }
-        };
-        request.open('GET', '/rest/api/2/issue/' + jiraKey + '?fields=issuetype,status', true);
-        request.send(); // retrieve issue details to close
+        }
     }
     
-    // closes isses following the JIRA workflow
-    function closeByWorkflow(jiraKey, type, status, complete) {
+    // closes issues following the JIRA workflow
+    async function closeByWorkflow(jiraKey, type, status, complete) {
         console.log('TYPE: ' + type);
         console.log('STATUS: ' + status);
 
-        const nextStatus = defineNextStatus(type, status);
+        const nextStatus = await defineNextStatus(type, status);
         console.log('NEXT STATUS: ' + nextStatus);
         if (nextStatus == 0) {
             return;
         }
 
-        let transitionsRequest = new XMLHttpRequest();
-        transitionsRequest.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                let transitionId = -1;
-                const transitionsReponse = JSON.parse(transitionsRequest.responseText);
-                // console.log(transitionsReponse);
-                transitionsReponse['transitions'].forEach(transition => {
-                    // looking for transition with target status
-                    if (transition['to']['id'] == nextStatus) {
-                        transitionId = transition['id'];
-                        // console.log(transitionId);
-                    }
-                });
-    
-                if (transitionId != -1) {
-                    let moveRequest = new XMLHttpRequest();
-                    moveRequest.onreadystatechange = function() {
-                        if (this.readyState == 4 && this.status == 204) {
-                            console.log(jiraKey + ' moved to ' + nextStatus);
-                            closeByWorkflow(jiraKey, type, nextStatus, complete); // follow the workflow until get closed
-                        }
-                    };
-                    moveRequest.open('POST', '/rest/api/2/issue/' + jiraKey + '/transitions?expand=transitions.fields', true);
-                    moveRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    
-                    // do the status change
+        const transitionsReponse = await fetch('/rest/api/2/issue/' + jiraKey + '/transitions'); // check available transitions
+        if (transitionsReponse.status == 200) {
+            const transitions = await transitionsReponse.json();
+            // console.log(transitions);
+            transitions['transitions'].forEach(transition => {
+                // looking for transition with target status
+                if (transition['to']['id'] == nextStatus) {
+                    const transitionId = transition['id'];
+                    // console.log(transitionId);
+                    let data = '';
                     if (nextStatus == STATUS_CLOSED) {
                         const resolution = (complete == true ? 'Done' : 'Won\'t Do');
-                        moveRequest.send(JSON.stringify({'fields': {'resolution': {'name': resolution}}, 'transition': {'id': transitionId}}));
+                        data = JSON.stringify({'fields': {'resolution': {'name': resolution}}, 'transition': {'id': transitionId}});
                     } else {
-                        moveRequest.send(JSON.stringify({'transition': {'id': transitionId}}));
+                        data = JSON.stringify({'transition': {'id': transitionId}});
                     }
+                    // do the status change
+                    const moveResponse = await fetch('/rest/api/2/issue/' + jiraKey + '/transitions?expand=transitions.fields', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json;charset=UTF-8'
+                        },
+                        body: data
+                    });
+                    // if (moveResponse.status == 204) {
+                    //     console.log(jiraKey + ' moved to ' + nextStatus);
+                    //     await closeByWorkflow(jiraKey, type, nextStatus, complete); // follow the workflow until get closed
+                    // }
                 }
-            }
-        };
-        transitionsRequest.open('GET', '/rest/api/2/issue/' + jiraKey + '/transitions', true);
-        transitionsRequest.send(); // check available transitions
+            });
+        }
     }
 
-    function defineNextStatus(type, status) {
+    async function defineNextStatus(type, status) {
         let nextStatus = 0;
         switch (Number(type)) {
             case TYPE_SUBTASK:
-                nextStatus = defineNextSubTaskStatus(status);
+                nextStatus = await defineNextSubTaskStatus(status);
                 break;
             case TYPE_USER_STORY:
             case TYPE_TEAM_ENABLER:
-                nextStatus = defineNextUserStoryStatus(status);
+                nextStatus = await defineNextUserStoryStatus(status);
                 break;
             case TYPE_TECHNICAL_TASK:
-                nextStatus = defineNextTechnialTaskStatus(status);
+                nextStatus = await defineNextTechnialTaskStatus(status);
                 break;
             case TYPE_EPIC:
-                nextStatus = defineNextEpicStatus(status);
+                nextStatus = await defineNextEpicStatus(status);
                 break;
         }
         return nextStatus;
     }
 
-    function defineNextSubTaskStatus(status) {
+    async function defineNextSubTaskStatus(status) {
         switch (Number(status)) {
             case STATUS_OPEN:
                 return STATUS_IN_PROGRESS;
@@ -198,7 +187,7 @@
         return 0;
     }
 
-    function defineNextUserStoryStatus(status) {
+    async function defineNextUserStoryStatus(status) {
         switch (Number(status)) {
             case STATUS_OPEN:
             case STATUS_READY_FOR_PUBLISH:
@@ -215,7 +204,7 @@
         return 0;
     }
 
-    function defineNextTechnialTaskStatus(status) {
+    async function defineNextTechnialTaskStatus(status) {
         switch (Number(status)) {
             case STATUS_OPEN:
             case STATUS_ACCEPTANCE:
@@ -230,7 +219,7 @@
         return 0;
     }
 
-    function defineNextEpicStatus(status) {
+    async function defineNextEpicStatus(status) {
         switch (Number(status)) {
             case STATUS_OPEN:
             case STATUS_ON_HOLD:
