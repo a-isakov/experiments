65c2dbec5abd0e60d1955d93fcfe1e2fc6e1ac06
Aleksey Isakov <alexx.id@gmail.com>
2021-10-17 09:50:12 +0300

    files in file

---
+++ python/Файлы в файле.py
@@ -0,0 +1,74 @@
+f = open('input.txt', encoding="utf-8")
+lines = f.readlines()
+f.close()
+
+groups = dict()
+extensions = []
+
+for line in lines:
+    values = line.rstrip().split(' ')
+    filename = values[0]
+    size = values[1]
+    size_multiplier = values[2]
+
+    if size_multiplier == 'B':
+        size = int(size)
+    elif size_multiplier == 'KB':
+        size = int(size) * 1024
+    elif size_multiplier == 'MB':
+        size = int(size) * 1024 * 1024
+    elif size_multiplier == 'GB':
+        size = int(size) * 1024 * 1024 * 1024
+    elif size_multiplier == 'TB':
+        size = int(size) * 1024 * 1024 * 1024 * 1024
+    else:
+        size = 0
+
+    name_values = filename.split('.')
+    name = name_values[0]
+    extension = name_values[1]
+
+    group = groups.get(extension)
+    if group is None:
+        extensions.append(extension)
+        group = []
+        group.append(size)
+        group.append([])
+        group[1].append(filename)
+        groups[extension] = group
+    else:
+        group[0] += size
+        group[1].append(filename)
+
+extensions.sort()
+f = open('output.txt', mode='w', encoding="utf-8")
+for extension in extensions:
+    group = groups[extension]
+    files = group[1]
+    files.sort()
+    for file in files:
+        file += '\n'
+        f.write(file)
+        # print(file)
+    f.write('----------\n')
+    # print('----------')
+    size = group[0]
+    size_multiplier = ''
+    if size < 1024:
+        size_multiplier = 'B\n'
+    elif size < 1024 * 1024:
+        size_multiplier = 'KB\n'
+        size = round(size / 1024)
+    elif size < 1024 * 1024 * 1024:
+        size_multiplier = 'MB\n'
+        size = round(size / 1024 / 1024)
+    elif size < 1024 * 1024 * 1024 * 1024:
+        size_multiplier = 'GB\n'
+        size = round(size / 1024 / 1024 / 1024)
+    elif size < 1024 * 1024 * 1024 * 1024 * 1024:
+        size_multiplier = 'TB\n'
+        size = round(size / 1024 / 1024 / 1024 / 1024)
+    # print('Summary:', size, size_multiplier)
+    summary = 'Summary: ' + str(size) + ' ' + size_multiplier + '\n'
+    f.write(summary)
+f.close()
