745717ecf245a95fb53dda84026701b18f15cdea
Aleksey Isakov <alexx.id@gmail.com>
2022-02-17 15:13:05 +0300

    minor

--- python/Результат тестирования.py
+++ python/Результат тестирования.py
@@ -3,4 +3,29 @@ import json
 
 with open('scoring.json') as cat_file:
     data = json.load(cat_file)
-    print(data)
+    # print(data)
+    tests_count = 0
+    for group in data['scoring']:
+        # print(group)
+        for n in group['required_tests']:
+            if n > tests_count:
+                tests_count = n
+    
+    # print(tests_count)
+    passed = []
+    for i in range(tests_count):
+        pass
+        answer = input()
+        if answer == 'ok':
+            passed.append(i + 1)
+    # print(passed)
+    passed = set(passed)
+
+    score = 0
+    for group in data['scoring']:
+        required = set(group['required_tests'])
+        common = required & passed
+        # if required & passed == required:
+        #     score += group['points']
+        score += group['points'] * len(common) // len(required)
+    print(score)
