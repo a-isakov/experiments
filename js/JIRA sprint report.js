9323e43d4205c39b81027944f958ccd50a27da63
Aleksey Isakov <alexx.id@gmail.com>
2022-11-02 09:58:07 +0300

    old code cleanup

--- js/JIRA sprint report.js
+++ js/JIRA sprint report.js
@@ -3,18 +3,6 @@ import 'google-apps-script'; // comment it before pasting to the google scripts
 // scroll down for report code
 
 const baseURL = 'https://tinypass.atlassian.net';
-const boardURLRow = 1;
-const boardURLColumn = 2;
-const progressRow = 2;
-const progressColumn = 2;
-const minDateRow = 3;
-const minDateColumn = 2;
-
-const configValueRow = 2;
-const configTypesCountColumn = 1;
-const configTypesSPColumn = 2;
-const configTableColumnColumn = 3;
-const configTableRowColumn = 4;
 
 const config = {
   mainSheet: {
@@ -40,11 +28,16 @@ const config = {
     titlesCol: 1,
     sprintsReportedRow: 1,
     sprintsReportedCol: 2,
-    // tableFirstRow: 3,
     tableFirstCol: 2,
     sprintRow: 3,
     datesRow: 4,
-    dataRow: 5
+    countTotalRow: 5,
+    countCompletedRow: 6,
+    countPercentRow: 7,
+    spTotalRow: 8,
+    spCompletedRow: 9,
+    spPercentRow: 10,
+    dataRow: 11
   }
 };
 
@@ -53,7 +46,6 @@ function onOpen(e) {
   SpreadsheetApp.getUi()
       .createMenu('Piano actions')
       .addItem('Run multi report', 'runMultiReport')
-      .addItem('Run report', 'runReport')
       .addItem('Refresh JIRA login', 'requestAccessString')
       .addToUi();
 }
@@ -144,7 +136,6 @@ function reportSubProgress(progressCell, progressText, color) {
 
 // -------------------------------------------------------------------
 // multi report code
-// scroll down for original report code
 function runMultiReport() {
   let mainSheet = SpreadsheetApp.getActive().getSheetByName(config.mainSheet.name);
   mainSheet.activate();
@@ -153,6 +144,11 @@ function runMultiReport() {
   reportProgress(progressCell, 'Running', 'black');
   reportSubProgress(subProgressCell, '', 'black');
   SpreadsheetApp.flush();
+
+  // sheet.getRange(tableRow, tableColumn, 1000, 1000).clear(); // clean old content before execution
+  // sheet.getRange(tableRow + 2, tableColumn - 1, 1000, 1)     // clean old content before execution
+  //   .clear()
+  //   .setFontWeight('bold');
 
   // read configuration
   const configSheet = SpreadsheetApp.getActive().getSheetByName(config.settingsSheet.name);
@@ -401,195 +397,4 @@ function getSprints(boardId, minDate, excludeSprints, teamConfig, sprints /*outp
   }
   sprints.sort((a, b) => (a.startDate > b.startDate) ? 1 : -1); // sort by date asc
   return true;
-}
-
-// -------------------------------------------------------------------
-// report code
-function runReport() {
-  let sheet = SpreadsheetApp.getActive().getSheetByName('Main-archive');
-  sheet.activate();
-  let boardCell = sheet.getRange(boardURLRow, boardURLColumn);
-  let progressCell = sheet.getRange(progressRow, progressColumn);
-  reportProgress(progressCell, 'Running', 'black');
-
-  // read configuration
-  const configSheet = SpreadsheetApp.getActive().getSheetByName('Configuration');
-  const tableColumn = configSheet.getRange(configValueRow, configTableColumnColumn).getValue();
-  const tableRow = configSheet.getRange(configValueRow, configTableRowColumn).getValue();
-  sheet.getRange(tableRow, tableColumn, 1000, 1000).clear(); // clean old content before execution
-  sheet.getRange(tableRow + 2, tableColumn - 1, 1000, 1)     // clean old content before execution
-    .clear()
-    .setFontWeight('bold');
-  let typesForCount = [];
-  let i = 0;
-  while (true) {
-    let typeForCount = configSheet.getRange(configValueRow + i, configTypesCountColumn).getValue().toString();
-    if (typeForCount == '') {
-      break;
-    }
-    typesForCount.push(typeForCount);
-    i++;
-  }
-  if (typesForCount.length == 0) {
-    reportProgress(progressCell, 'Failed: No types for count', 'red');
-    return;
-  }
-  let typesForSP = [];
-  i = 0;
-  while (true) {
-    let typeForSP = configSheet.getRange(configValueRow + i, configTypesSPColumn).getValue().toString();
-    if (typeForSP == '') {
-      break;
-    }
-    typesForSP.push(typeForSP);
-    i++;
-  }
-  if (typesForSP.length == 0) {
-    reportProgress(progressCell, 'Failed: No types for SP', 'red');
-    return;
-  }
-  // define configured rows
-  let rowIndex = tableRow;
-  const sprintsRow = rowIndex++;
-  const sprintDatesRow = rowIndex++;
-  let configTypesForSP = {}; // keeps row numbers for each type to report SP
-  for (let i = 0; i < typesForSP.length; i++) {
-    configTypesForSP[typesForSP[i]] = {'countRow': rowIndex++};
-    configTypesForSP[typesForSP[i]]['completedRow'] = rowIndex++;
-    configTypesForSP[typesForSP[i]]['completedPercentRow'] = rowIndex++;
-    sheet.getRange(configTypesForSP[typesForSP[i]].countRow, 1).setValue(typesForSP[i] + ' SP');
-    sheet.getRange(configTypesForSP[typesForSP[i]].completedRow, 1).setValue(typesForSP[i] + ' SP completed');
-    sheet.getRange(configTypesForSP[typesForSP[i]].completedPercentRow, 1).setValue(typesForSP[i] + ' SP %');
-  }
-  let configTypesForCount = {}; // keeps row numbers for each type to report count of issues
-  for (let i = 0; i < typesForCount.length; i++) {
-    configTypesForCount[typesForCount[i]] = {'countRow': rowIndex++};
-    configTypesForCount[typesForCount[i]]['completedRow'] = rowIndex++;
-    configTypesForCount[typesForCount[i]]['completedPercentRow'] = rowIndex++;
-    sheet.getRange(configTypesForCount[typesForCount[i]].countRow, 1).setValue(typesForCount[i]);
-    sheet.getRange(configTypesForCount[typesForCount[i]].completedRow, 1).setValue(typesForCount[i] + ' completed');
-    sheet.getRange(configTypesForCount[typesForCount[i]].completedPercentRow, 1).setValue(typesForCount[i] + ' %');
-  }
-
-  // check connectivity
-  if (!pingJira()) {
-    reportProgress(progressCell, 'Failed: Refresh JIRA login', 'red');
-    return;
-  }
-  const boardURL = boardCell.getValue().toString();
-  if (boardURL == '') {
-    reportProgress(progressCell, 'Failed: Board URL is empty', 'red');
-    return;
-  }
-  const boardId = getBoardId(boardURL);
-  if (boardId == -1) {
-    reportProgress(progressCell, 'Failed: Bad Board URL', 'red');
-    return;
-  }
-
-  // collect all sprints for the board
-  const minDate = sheet.getRange(minDateRow, minDateColumn).getValue();
-  let sprints = [];
-  let startAt = 0;
-  while (true) {
-    let apiURL = '/rest/agile/1.0/board/' + boardId + '/sprint?state=closed&startAt=' + startAt;
-    // Logger.log(apiURL);
-    let response = fetchJira(apiURL);
-    if (response.getResponseCode() < 200 || response.getResponseCode() >= 300) {
-      reportProgress(progressCell, 'Failed: Cannot retrieve sprints from board', 'red');
-      return;
-    }
-    let jsonResponse = JSON.parse(response.getContentText());
-    let values = jsonResponse.values;
-    for (let i = 0; i < values.length; i++) {
-      const sprintStartDate = new Date(values[i].startDate.substring(0, 10));
-      if (sprintStartDate >= minDate) {
-        sprints.push(values[i]);
-      }
-    }
-    if (jsonResponse.isLast) {
-      break;
-    }
-    startAt += parseInt(jsonResponse.maxResults);
-  }
-  sprints.sort((a, b) => (a.startDate > b.startDate) ? 1 : -1); // sort by date asc
-  // sprints.sort((a, b) => (a.startDate > b.startDate) ? -1 : 1); // sort by date desc
-  for (let i = 0; i < sprints.length; i++) {
-    reportProgress(progressCell, 'Scanning sprints ' + parseInt((i + 1)*100/sprints.length) + '%', 'black');
-    let sprint = sprints[i];
-    sheet.getRange(sprintsRow, tableColumn + i).setValue(sprint.name);
-    sheet.getRange(sprintDatesRow, tableColumn + i).setValue(sprint.startDate.substring(0, 10) + ' - ' + sprint.endDate.substring(0, 10));
-    sheet.autoResizeColumn(tableColumn + i);
-
-    // reset counters
-    let counter = {};
-    for (let j = 0; j < typesForSP.length; j++) {
-      counter[typesForSP[j]] = {'count': 0, 'completed': 0, 'SP': 0, 'completedSP': 0};
-    }
-    for (let j = 0; j < typesForCount.length; j++) {
-      counter[typesForCount[j]] = {'count': 0, 'completed': 0, 'SP': 0, 'completedSP': 0};
-    }
-    // get all issues
-    let apiURL = '/rest/greenhopper/1.0/rapid/charts/sprintreport?rapidViewId=' + boardId + '&sprintId=' + sprint.id;
-    let response = fetchJira(apiURL);
-    if (response.getResponseCode() < 200 || response.getResponseCode() >= 300) {
-      reportProgress(progressCell, 'Failed: Cannot retrieve issues for sprint ' + sprint.name, 'red');
-      return;
-    }
-    let jsonResponse = JSON.parse(response.getContentText());
-    let completedIssues = jsonResponse.contents.completedIssues;
-    for (let j = 0; j < completedIssues.length; j++) {
-      let completedIssue = completedIssues[j];
-      let type = completedIssue.typeName;
-      if (typesForCount.includes(type)) {
-        counter[type].count++;
-        counter[type].completed++;
-      }
-      if (typesForSP.includes(type)) {
-        const sp = getIssueSP(completedIssue.key);
-        counter[type].completedSP += sp;
-        counter[type].SP += sp;
-      }
-    }
-    let notCompletedIssues = jsonResponse.contents.issuesNotCompletedInCurrentSprint;
-    for (let j = 0; j < notCompletedIssues.length; j++) {
-      let notCompletedIssue = notCompletedIssues[j];
-      let type = notCompletedIssue.typeName;
-      if (typesForCount.includes(type)) {
-        counter[type].count++;
-      }
-      if (typesForSP.includes(type)) {
-        const sp = getIssueSP(notCompletedIssue.key);
-        counter[type].SP += sp;
-      }
-    }
-
-    // write counters
-    for (let j = 0; j < typesForSP.length; j++) {
-      sheet.getRange(configTypesForSP[typesForSP[j]].countRow, tableColumn + i)
-        .setValue(counter[typesForSP[j]].SP)
-        .setNumberFormat('0');
-      sheet.getRange(configTypesForSP[typesForSP[j]].completedRow, tableColumn + i)
-        .setValue(counter[typesForSP[j]].completedSP)
-        .setNumberFormat('0');
-      sheet.getRange(configTypesForSP[typesForSP[j]].completedPercentRow, tableColumn + i)
-        .setFormulaR1C1('=R[-1]C[0]/R[-2]C[0]')
-        .setNumberFormat('0.#%');
-    }
-    for (let j = 0; j < typesForCount.length; j++) {
-      sheet.getRange(configTypesForCount[typesForCount[j]].countRow, tableColumn + i)
-        .setValue(counter[typesForCount[j]].count)
-        .setNumberFormat('0');
-      sheet.getRange(configTypesForCount[typesForCount[j]].completedRow, tableColumn + i)
-        .setValue(counter[typesForCount[j]].completed)
-        .setNumberFormat('0');
-      sheet.getRange(configTypesForCount[typesForCount[j]].completedPercentRow, tableColumn + i)
-        .setFormulaR1C1('=R[-1]C[0]/R[-2]C[0]')
-        .setNumberFormat('0.#%');
-    }
-    // break;
-    SpreadsheetApp.flush();
-  }
-
-  reportProgress(progressCell, 'Finished', 'green');
 }
