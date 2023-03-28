2ee72aac2373a9de602237a2933f2ddc8e72c072
a-isakov <alexx.id@gmail.com>
2023-03-28 17:16:27 +0300

    style fix

--- python/tasks.py
+++ python/tasks.py
@@ -27,15 +27,44 @@ def convert_to_dec(n, s):
         res += n[i]*(s**i)
     return res
 
-n1 = [2, 5, 1, 3, 1]
-# print(convert_to_dec(n1, 16))
-# print(int('13152', 16))
-# n2 = [5, 2, x, 7]
-counter = 0
-for x in range(6, 100):
-    n2 = [5, 2]
-    n2.append(x)
-    n2.append(7)
-    if (convert_to_dec(n1, x) + convert_to_dec(n2, 100)) % 11 == 0:
-        counter += 1
-print(counter)
+# n1 = [2, 5, 1, 3, 1]
+# # print(convert_to_dec(n1, 16))
+# # print(int('13152', 16))
+# # n2 = [5, 2, x, 7]
+# counter = 0
+# for x in range(6, 100):
+#     n2 = [5, 2]
+#     n2.append(x)
+#     n2.append(7)
+#     if (convert_to_dec(n1, x) + convert_to_dec(n2, 100)) % 11 == 0:
+#         counter += 1
+# print(counter)
+
+# n1 = [ord('Z')- ord('A') + 10, a, ord('Y')- ord('A') + 10, ord('X')- ord('A') + 10]
+# n2 = [2, ord('X')- ord('A') + 10, a, ord('Y')- ord('A') + 10]
+result_array = []
+for a in range(55):
+    n1 = [33, 34, a, 35]
+    n2 = [34, a, 33, 2]
+    res = convert_to_dec(n1, 55) - convert_to_dec(n2, 55)
+    if res % 29 == 0:
+        result_array.append(a)
+
+min_a = min(result_array)
+print('min a =', min_a)
+
+n1 = [33, 34, min_a, 35]
+n2 = [34, min_a, 33, 2]
+r1 = abs(convert_to_dec(n1, 55) - convert_to_dec(n2, 55))
+print(r1) # ZaYX55 –  2XaY55
+
+max_a = max(result_array)
+print('max a =', max_a)
+
+n1 = [33, 34, max_a, 35]
+n2 = [34, max_a, 33, 2]
+r2 = abs(convert_to_dec(n1, 55) - convert_to_dec(n2, 55))
+print(r2) # ZaYX55 –  2XaY55
+
+result = abs(r1 - r2)
+print(result)
