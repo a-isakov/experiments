032e43b6406ea9cd3b27ff39e93d3e055a01f093
a-isakov <alexx.id@gmail.com>
2024-05-24 09:38:22 +0200

    minor

--- python/sf-sample.py
+++ python/sf-sample.py
@@ -131,7 +131,7 @@ dates = []
 # dates.append((startDate, endDate))
 # print(dates)
 # manual array composition
-dates.append(("2024-05-22", "2024-05-23"))
+dates.append(("2024-05-23", "2024-05-24"))
 
 print("API Key:")
 apiKey = input()
