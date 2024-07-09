d882f48ccc3384824ef8548f8181a3dacf68198e
a-isakov <alexx.id@gmail.com>
2024-07-09 10:09:07 +0300

    style change

--- js/JIRA focus board.js
+++ js/JIRA focus board.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JIRA focus board
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      2.1
 // @description  hide unnecessary elements
 // @author       You
 // @match        https://tinypass.atlassian.net/jira/*
@@ -36,7 +36,7 @@
         expandButton.className = 'css-178ag6o';
         expandButton.innerHTML = '<svg width="20" height="20" viewBox="0 -4 24 24" role="presentation"><path d="M16.587 6.003H15A1 1 0 0115 4h3.9l.047.001a.975.975 0 01.736.285l.032.032c.2.2.296.47.284.736l.001.048v3.896a1 1 0 11-2 0V7.411l-3.309 3.308a.977.977 0 01-1.374-.005l-.032-.032a.976.976 0 01-.005-1.374l3.307-3.305zM7.413 17.997H9A1 1 0 019 20H5.1l-.047-.001a.975.975 0 01-.736-.285l-.032-.032A.977.977 0 014 18.946a1.12 1.12 0 010-.048v-3.896a1 1 0 112 0v1.587l3.309-3.308a.977.977 0 011.374.005l.032.032a.976.976 0 01.005 1.374l-3.307 3.305z" fill="currentColor" fill-rule="evenodd"></path></svg>';
         let expandButtonContainer = document.createElement('label');
-        expandButtonContainer.className = '_uiztglyw css-1luyhz2';
+        expandButtonContainer.className = '_uiztglyw css-1l34k60';
         expandButtonContainer.appendChild(expandButton);
         expandButtonContainer.addEventListener('click', function() {
             customExpandListener()
