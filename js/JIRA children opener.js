7861595b326b0fb5e4885e1582864a46196c0dd6
a-isakov <alexx.id@gmail.com>
2024-12-19 18:37:46 +0300

    styles adoption

--- js/JIRA children opener.js
+++ js/JIRA children opener.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JIRA children opener
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  enables to open children tasks
 // @author       You
 // @match        https://tinypass.atlassian.net/browse/*
@@ -33,10 +33,11 @@
 
                 let openerDiv = document.createElement('div');
                 openerDiv.setAttribute('id', FAKE_ID); // insert unique div to prevent duplicated execution
-                openerDiv.setAttribute('class', '_18u012x7');
+
+                const styles = await getStyles();
 
                 let openerButton = document.createElement('button');
-                openerButton.setAttribute('class', 'css-181az2k'); // copied from [+] button
+                openerButton.setAttribute('class', styles.button);
                 openerButton.setAttribute('type', 'button');
                 openerButton.addEventListener('click', function() {
                     customChildrenOpenListener(sortButton.parentNode.parentNode.parentNode)
@@ -44,11 +45,11 @@
                 openerDiv.appendChild(openerButton);
 
                 let openerSpan = document.createElement('span');
-                openerSpan.setAttribute('class', 'css-1gjw6ge'); // copied from [+] button
+                openerSpan.setAttribute('class', styles.span);
                 openerButton.appendChild(openerSpan);
 
                 let imageSpan = document.createElement('span');
-                imageSpan.setAttribute('class', 'css-1wits42');
+                imageSpan.setAttribute('class', styles.subSpan);
                 imageSpan.innerHTML += '<svg width="24" height="24" viewBox="0 0 24 24" role="presentation"><g fill="currentColor"><path d="M19.005 19c-.003 0-.005.002-.005.002l.005-.002zM5 19.006c0-.004-.002-.006-.005-.006H5v.006zM5 4.994V5v-.006zM19 19v-6h2v6.002A1.996 1.996 0 0119.005 21H4.995A1.996 1.996 0 013 19.006V4.994C3 3.893 3.896 3 4.997 3H11v2H5v14h14zM5 4.994V5v-.006zm0 14.012c0-.004-.002-.006-.005-.006H5v.006zM11 5H5v14h14v-6h2v6.002A1.996 1.996 0 0119.005 21H4.995A1.996 1.996 0 013 19.006V4.994C3 3.893 3.896 3 4.997 3H11v2zm8 0v3a1 1 0 002 0V4a1 1 0 00-1-1h-4a1 1 0 000 2h3z"></path><path d="M12.707 12.707l8-8a1 1 0 10-1.414-1.414l-8 8a1 1 0 001.414 1.414z"></path></g></svg>';
                 openerSpan.appendChild(imageSpan);
 
@@ -57,14 +58,32 @@
         }
     }
 
+    async function getStyles() {
+        let styles = {button: '', span: '', subSpan: ''};
+        // find [+] button to copy styles
+        const plusButton = document.querySelector('[data-testid="issue-view-common-views.button.icon-button.Create child"]');
+        if (plusButton != null) {
+            styles.button = plusButton.getAttribute('class');
+            const span = plusButton.childNodes[0];
+            if (span != null) {
+                styles.span = span.getAttribute('class');
+                const subSpan = span.childNodes[0];
+                if (subSpan != null) {
+                    styles.subSpan = subSpan.getAttribute('class');
+                }
+            }
+        }
+        return styles;
+    }
+
     function customChildrenOpenListener(childrenPanel) {
         if (confirm('Are you sure you want to open all children?')){
             const children = childrenPanel.querySelector('[data-testid="issue.issue-view.views.common.child-issues-panel.issues-container"]');
-            const lines = children.getElementsByClassName('w4juse-1 lmnboH');
-            for (let i = 0; i < lines.length; i++) {
-                const line = lines[i];
-                const key = line.querySelector('[data-testid="issue.issue-view.views.common.issue-line-card.issue-line-card-view.key"]');
-                window.open('https://tinypass.atlassian.net/browse/' + key.textContent);
+            if (children != null) {
+                const keys = children.querySelectorAll('[data-testid="issue.issue-view.views.common.issue-line-card.issue-line-card-view.key"]');
+                if (keys != null) {
+                    keys.forEach(key => window.open('https://tinypass.atlassian.net/browse/' + key.textContent));
+                }
             }
         }
     }
